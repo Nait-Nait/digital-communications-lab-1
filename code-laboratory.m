4819ff0473374216c0e57d64b295cc521f1604f0
Nait-Nait <naitsab_algorithms@hotmail.com>
2025-04-08 19:34:51 -0400

    Update code-laboratory.m

--- code-laboratory.m
+++ code-laboratory.m
@@ -55,15 +55,13 @@ legend;
 title('Comparación de Muestreos: Natural vs. Instantáneo');
 
 
-% Fourier Transform
+% Transformadas de Fourier
 M_t = fft(m_t);
 M_t_nat = fft(m_t_nat);
 M_t_inst = fft(m_t_inst);
 
-% Frequency axis for plotting
 f_axis = (0:(length(t) - 1)) * (1 / (ls * tm));
 
-% Create the first figure with original, natural, and instantaneous signals
 figure;
 subplot(3, 1, 1);
 plot(t, m_t);
@@ -83,7 +81,6 @@ title('Muestreo Instantáneo');
 xlabel('Tiempo (s)');
 ylabel('Amplitud');
 
-% Create the second figure with Fourier Transforms
 figure;
 subplot(3, 1, 1);
 plot(f_axis, abs(M_t));
@@ -103,23 +100,18 @@ title('Transformada de Fourier Muestreo Instantáneo');
 xlabel('Frecuencia (Hz)');
 ylabel('Magnitud');
 
-% PCM Parameters
 bit_depth = 8; 
 pcm_levels = 2^bit_depth;
 
-% Quantize the instantaneous signal using PCM
 pcm_signal_inst = round((m_t_inst + 1) * (pcm_levels - 1) / 2);
 grid on;
 
-% Normaliza las señales para que estén en el mismo rango de amplitud (0 a 1)
 m_t_norm = (m_t - min(m_t)) / (max(m_t) - min(m_t));
 m_t_inst_norm = (m_t_inst - min(m_t_inst)) / (max(m_t_inst) - min(m_t_inst));
 pcm_signal_inst_norm = (pcm_signal_inst - min(pcm_signal_inst)) / (max(pcm_signal_inst) - min(pcm_signal_inst));
 
-% Calcular el error de cuantización para la señal PAM cuantificada (PCM)
 quantization_error_inst = m_t_inst - ((2 * pcm_signal_inst / (pcm_levels - 1)) -1);
 
-% Crear una figura para mostrar todas las señales en un mismo gráfico
 figure;
 plot(t, m_t_norm, 'b', 'LineWidth', 1.5); 
 hold on;
@@ -132,7 +124,6 @@ title('Señal Original, Señal PAM Instantánea y Señal PAM Cuantificada (PCM)'
 legend('Señal Original', 'Señal PAM Instantánea', 'Señal PAM Cuantificada (PCM)');
 grid on;
 
-% Crear una nueva figura para mostrar el error de cuantización
 figure;
 plot(t, quantization_error_inst, 'k--', 'LineWidth', 1.5);
 
